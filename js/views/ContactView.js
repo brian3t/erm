4eddb575d6e0e091b3c84ea9cf0fb80051640e0e
brian3t <ngxtri@gmail.com>
2016-11-05 16:41:17 -0700

    Working on Contacts..

--- js/views/ContactView.js
+++ js/views/ContactView.js
@@ -14,38 +14,7 @@ app.views.ContactView = Backbone.View.extend({
     },
 
     events: {
-        "click .logout": "back",
-        "blur .edit": "update_ajax",
-        "change .multi_select": "update_ajax",
-    },
-    update_ajax: function (e) {
-        var target = $(e.target);
-        if (target.hasClass('file-caption') || target.prop('type') == 'file') {
-            return;
-        }
-        var form = target.parents('form');
-        var model = form.data('model');//user
-        new_attr = {};
-        new_attr[target.prop('name')] = target.val();
-        target.before('<span class="glyphicon glyphicon-upload"></span>');
-        app[model].save(new_attr, {
-            patch: true, success: function () {
-                target.prev('span.glyphicon-upload').remove();
-                //dont bother if it's multiselect
-                if (target.hasClass('multi_select') || target.hasClass('select2-search__field') || target.hasClass('select2-selection--multiple')){
-                    return;
-                }
-                target.before('<span class="glyphicon glyphicon-ok-circle"></span>');
-                setTimeout(function () {
-                    target.prev('span').fadeOut(1400);
-                }, 2000);
-            }, error: function () {
-                target.prev('span.glyphicon-upload').remove();
-            }
-        });
-    },
-    update_ajax_multi: function (e) {
-
+        "click .logout": "back"
     },
 
     dom_ready: function () {
--- js/views/UserListView.js
+++ js/views/UserListView.js
@@ -5,6 +5,7 @@ app.views.UserListView = Backbone.View.extend({
     tagName: 'div',
     className: 'user_list_view',
     collection: {},
+    cur_model_index: 0,//current model that is being selected out of collection
     initialize: function () {
         var self = this;
         this.collection = new app.models.User_collection();
@@ -12,7 +13,9 @@ app.views.UserListView = Backbone.View.extend({
         this.user_search_list_view = new app.views.UserSearchListView({collection: this.collection});
         this.user_form_view = new app.views.UserView();
         this.listenTo(this.collection, 'update', this.render);
+        this.delegateEvents();
     },
+
     user_form_view: {},
     user_search_list_view: {},
     render: function () {
@@ -21,13 +24,16 @@ app.views.UserListView = Backbone.View.extend({
         var first_user = this.collection.first();
         this.$el.html(this.template());
         if (_.isObject(first_user)) {
+            this.cur_model_index = 0;
             this.user_form_view.model = first_user;
-            this.user_form_view.bind_event();
-            this.$('#user_form').html(this.user_form_view.render().el);
+            $('#user_form_wrapper').html(this.user_form_view.render());
+            this.user_form_view.delegateEvents();
         }
         //todob dont use global id in selector
         $('#user_search_list').html(this.user_search_list_view.render());
+        this.user_search_list_view.delegateEvents();
         return this.$el.html();
+
     }
 });
 
@@ -37,7 +43,7 @@ app.views.UserSearchListView = Backbone.View.extend({
     className: "table-responsive",
 
     initialize: function () {
-        this.listenTo(this.collection, 'change reset add remove', this.render)
+        this.listenTo(this.collection, 'change reset add remove', this.render);
         this.listenTo(this.collection, 'destroy', this.close)
     },
 
@@ -53,18 +59,61 @@ app.views.UserView = Backbone.View.extend({
     tagName: "div",
     id: "user_form",
     model: app.models.User,
-    className: "col-md-10 edit account_info",
+    // className: "col-md-10 edit account_info",
+    events: {
+        "blur .edit": "update_ajax",
+        "change .multi_select": "update_ajax",
+        "blur input[name='first_name']": "fired",
+        "change .edit_switch": "toggle_edit_mode"
+    },
+    fired: function (e) {
+        console.log("Event fired");
+        console.info(e);
+    },
+    update_ajax: function (e) {
+        var target = $(e.target);
+        if (target.hasClass('file-caption') || target.prop('type') == 'file') {
+            return;
+        }
+        var form = target.parents('form');
+        var model = form.data('model');//user
+        new_attr = {};
+        new_attr[target.prop('name')] = target.val();
+        target.before('<span class="glyphicon glyphicon-upload"></span>');
+        this.model.save(new_attr, {
+            patch: true, success: function () {
+                target.prev('span.glyphicon-upload').remove();
+                //dont bother if it's multiselect
+                if (target.hasClass('multi_select') || target.hasClass('select2-search__field') || target.hasClass('select2-selection--multiple')){
+                    return;
+                }
+                target.before('<span class="glyphicon glyphicon-ok-circle"></span>');
+                setTimeout(function () {
+                    target.prev('span').fadeOut(1400);
+                }, 2000);
+            }, error: function () {
+                target.prev('span.glyphicon-upload').remove();
+            }
+        });
+    },
+    update_ajax_multi: function (e) {
 
+    },
+    toggle_edit_mode: function (e) {
+        var $e = $(e.currentTarget);
+
+    },
     initialize: function () {
+        this.delegateEvents();
     },
-    bind_event: function () {
-        this.model.on("add change", this.render, this);
-        this.model.on("destroy", this.close, this);
-    },
+    // bind_event: function () {
+    //     this.model.on("add change", this.render, this);
+    //     this.model.on("destroy", this.close, this);
+    // },
 
     render: function () {
         //todob dont use global selector
-        $('#user_form').html(this.template(this.model.attributes));
-        return this.$el.html();
+        this.$el.html(this.template(this.model.attributes));
+        return this.$el;
     }
 });
