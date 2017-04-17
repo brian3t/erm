f1c3714928f7d5e44b323bd2eb944611423fc316
brian3t <ngxtri@gmail.com>
2017-04-17 14:29:13 -0700

    Fix created at updated at null object.

--- js/views/CompanyListView.js
+++ js/views/CompanyListView.js
@@ -15,10 +15,10 @@ app.views.CompanyListView = Backbone.View.extend({
 
     initialize: function () {
         this.collection = app.collections.companies;
-        this.collection.models = this.collection.filter(function (v) {
+        /*this.collection.models = this.collection.filter(function (v) {
             return true;
             // return v.id == app.cur_user.get('company').get('id');
-        });
+        });*/
         this.company_search_list_view = new app.views.CompanySearchListView({collection: this.collection});
         this.company_form_view = new app.views.CompanyView();
         this.listenTo(this.collection, 'update', this.render);
